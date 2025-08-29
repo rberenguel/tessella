db9021dfcbaeeb6d4f82b8431090ebdeb6c2f6eb
Ruben Berenguel <ruben@mostlymaths.net>
2025-08-29 18:57:56 +0200

    Update canvas.js
    Sometimes I'm no better than the LLM

--- canvas.js
+++ canvas.js
@@ -750,8 +750,8 @@ async function init() {
     } else {
       document.body.classList.remove("landscape");
       if (canvas.height != portraitHeight) {
-        canvas.height = portraitWidth;
-        canvas.width = portraitHeight;
+        canvas.width = portraitWidth;
+        canvas.height = portraitHeight;
       }
     }
   }, 500);
